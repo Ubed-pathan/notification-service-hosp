21fae826b515d1c2a786af185d14cd9f5df65e0b
Ubed-pathan <ubedpathan818@gmail.com>
2025-08-17 16:27:46 +0530

    added cancel and complete listener in notification service

--- src/main/java/com/appointment/notification_service/listener/AppointmentEventListener.java
+++ src/main/java/com/appointment/notification_service/listener/AppointmentEventListener.java
@@ -33,6 +33,54 @@ public class AppointmentEventListener {
         );
 
         mailService.sendMail(event.userEmail(), subject, body);
-//        System.out.println("✅ Email sent to: " + event.userEmail());
+    }
+
+
+
+    @KafkaListener(
+            topics = "appointments.cancelled",
+            groupId = "${KAFKA_GROUP_ID}",
+            containerFactory = "kafkaListenerContainerFactory"
+    )
+    public void handleAppointmentCancelled(AppointmentCreatedEvent event) {
+        if (event.userEmail() == null || event.userEmail().isBlank()) {
+            return;
+        }
+
+        String subject = "Appointment Cancelled - " + event.appointmentId();
+        String body = String.format(
+                "Hello %s,\n\nYour appointment with Doctor %s scheduled for %s has been cancelled.\nReason: %s\n\nRegards,\nClinic Team",
+                event.usersFullName(),
+                event.doctorFullName(),
+                event.appointmentTime(),
+                event.reason()
+        );
+
+        mailService.sendMail(event.userEmail(), subject, body);
+    }
+
+
+
+
+    @KafkaListener(
+            topics = "appointments.completed",
+            groupId = "${KAFKA_GROUP_ID}",
+            containerFactory = "kafkaListenerContainerFactory"
+    )
+    public void handleAppointmentCompleted(AppointmentCreatedEvent event) {
+        if (event.userEmail() == null || event.userEmail().isBlank()) {
+            return;
+        }
+
+        String subject = "Appointment Completed - " + event.appointmentId();
+        String body = String.format(
+                "Hello %s,\n\nYour appointment with Doctor %s on %s has been marked as completed.\nReason: %s\n\nThank you for visiting.\nClinic Team",
+                event.usersFullName(),
+                event.doctorFullName(),
+                event.appointmentTime(),
+                event.reason()
+        );
+
+        mailService.sendMail(event.userEmail(), subject, body);
     }
 }
